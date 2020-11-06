42afdd26480587b4f9c5372a1115761e5920b980
Dimitris Lamprinos <pkakelas@gmail.com>
2020-11-06 13:34:49 +0200

    Implement divide function

--- index.js
+++ index.js
@@ -9,3 +9,10 @@ const multiply = (a, b) => {
 const subtract = (a, b) => {
     return a - b;
 }
+
+const divide = (a, b) => {
+    if (b == 0) {
+	throw Error("Divide by 0 is not permitted");
+    }
+    return a / b;
+}
